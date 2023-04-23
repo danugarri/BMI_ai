2671c537766fc14c3cf3b94587746a4ac2e7ecca
danugarri <danugarri@hotmail.com>
2023-04-23 20:41:02 +0200

    css

--- src/components/result/ResultViewer.tsx
+++ src/components/result/ResultViewer.tsx
@@ -1,6 +1,7 @@
 import { INeuralNetworkData } from 'brain.js/dist/neural-network';
 import React from 'react';
 import { fatties, fit, skinnies } from '../../assets/';
+import './ResultViewer.css';
 
 export const ResultViewer: React.FC<{ bmi: number; result: INeuralNetworkData }> = ({
     bmi,
@@ -12,13 +13,19 @@ export const ResultViewer: React.FC<{ bmi: number; result: INeuralNetworkData }>
             : !!result && result.toString() === 'fat'
             ? fatties
             : skinnies;
+    const text =
+        !!result && result.toString() === 'fit'
+            ? 'Fit'
+            : !!result && result.toString() === 'fat'
+            ? 'Sobrepeso'
+            : 'Bajo peso';
     return (
         <>
             {result && (
-                <div>
+                <div className="result-container">
                     <>
                         <p>Índice de masa corporal: {bmi}</p>
-                        {result}
+                        <p className="body-text">{text}</p>
                         <img alt="bodies" src={body} />
                     </>
                 </div>
